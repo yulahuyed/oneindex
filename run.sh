d691fcf123885ebcf6a90e5abed9dc5bcf23040c
yulahuyed <yulahuyed@vpsorg.top>
2018-10-06 21:41:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,17 @@
 if [ "${DOMAIN}" ]
 then
   sed -i "s#https://ju.tn/#${DOMAIN}#g" /home/oneindex/view/admin/install/install_1.php
+  sed -i "s#ju.tn#${DOMAIN}#g" /home/oneindex/view/admin/install/install_1.php
   sed -i "s#https://ju.tn/#${DOMAIN}#g" /home/oneindex/controller/AdminController.php
+fi
+
+if [ "${ADMIN_PASS}" ]
+then
+  sed -i "s#:oneindex)#:${ADMIN_PASS})#g" /home/oneindex/view/admin/install/install_3.php
+  sed -i "s#'password' => 'oneindex#'password' => '${ADMIN_PASS}#g" /home/oneindex/controller/AdminController.php
+else
+  sed -i "s#:oneindex)#:yhiblog)#g" /home/oneindex/view/admin/install/install_3.php
+  sed -i "s#'password' => 'oneindex'#'password' => 'yhiblog'#g" /home/oneindex/controller/AdminController.php
 fi
 
 php-fpm7 -D
