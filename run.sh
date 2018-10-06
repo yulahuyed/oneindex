0b06f945651f62bb45a16eb02b2e0409d6c8144f
yulahuyed <yulahuyed@vpsorg.top>
2018-10-06 21:25:16 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "${DOMAIN}" ]
+then
+  sed -i "s#https://ju.tn/#${DOMAIN}#g" /home/oneindex/view/admin/install/install_1.php
+  sed -i "s#https://ju.tn/#${DOMAIN}#g" /home/oneindex/controller/AdminController.php
+fi
+
 php-fpm7 -D
 echo "0 * * * * php /home/oneindex/one.php token:refresh" > /home/oneindex/crontab
 echo "*/1 * * * * php /home/oneindex/one.php cache:refresh" >> /home/oneindex/crontab
